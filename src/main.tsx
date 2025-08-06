8312df660f6e93aecbab6bb34e48f4bbb98cbbd4
tschickel22 <109991351+tschickel22@users.noreply.github.com>
2025-08-06 11:59:15 -0600

    Fix JSX syntax error in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async'
-import * as pdfjs from 'pdfjs-dist'
-import PdfWorker from 'pdfjs-dist/build/pdf.worker.min.mjs?url'
 import App from './App.tsx'
 import './index.css'
 
-// Configure PDF.js worker
-pdfjs.GlobalWorkerOptions.workerSrc = PdfWorker
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <HelmetProvider>
       <App />
     </HelmetProvider>
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <App />
+  </React.StrictMode>
 )
