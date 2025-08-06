70e7da4ab0c82c4326deb2db41a578d79db774c2
tschickel22 <109991351+tschickel22@users.noreply.github.com>
2025-08-06 16:16:50 -0600

    Updated LandManagement.tsx

--- src/modules/land-management/LandManagement.tsx
+++ src/modules/land-management/LandManagement.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import LandDashboard from './components/LandDashboard'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import LandList from './components/LandList'
 import LandForm from './components/LandForm'
 import LandDetail from './components/LandDetail'
@@ -9,11 +8,20 @@ export default function LandManagement() {
   return (
     <div className="space-y-6">
       <Routes>
-        <Route path="/" element={<LandDashboard />} />
-        <Route path="/list" element={<LandList />} />
+        {/* Main land list view */}
+        <Route path="/" element={<LandList />} />
+        
+        {/* Create new land record */}
         <Route path="/new" element={<LandForm />} />
+        
+        {/* Edit existing land record */}
         <Route path="/edit/:id" element={<LandForm />} />
-        <Route path="/detail/:id" element={<LandDetail />} />
+        
+        {/* View land record details */}
+        <Route path="/:id" element={<LandDetail />} />
+        
+        {/* Redirect any unmatched routes to the main list */}
+        <Route path="*" element={<Navigate to="/land" replace />} />
       </Routes>
     </div>
   )
