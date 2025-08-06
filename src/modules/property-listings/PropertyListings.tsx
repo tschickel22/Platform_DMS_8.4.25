a9e397c58e932650e1e2be8a79db2f447612caa9
tschickel22 <109991351+tschickel22@users.noreply.github.com>
2025-08-06 12:38:22 -0600

    Remove extraneous text from beginning of file

--- src/modules/property-listings/PropertyListings.tsx
+++ src/modules/property-listings/PropertyListings.tsx
@@ -1,4 +1,4 @@
-can you rewrite and fix this issue?import React, { useState } from 'react'
+import React, { useState } from 'react'
 import { Routes, Route, useNavigate, useLocation } from 'react-router-dom'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 import { Button } from '@/components/ui/button'
