3b047e20c798059be08441c4cc8bf3ffd2d17892
tschickel22 <109991351+tschickel22@users.noreply.github.com>
2025-08-11 17:30:08 -0600

    Updated PropertyListings.tsx

--- src/modules/property-listings/PropertyListings.tsx
+++ src/modules/property-listings/PropertyListings.tsx
@@ -1,21 +1,19 @@
 import React from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import ListingsWorkspace from './components/ListingsWorkspace'
 import ListingOverview from './components/ListingOverview'
+import ListingForm from './components/ListingForm'
 
 export default function PropertyListings() {
   return (
-    <div className="flex flex-col h-full">
-    <Routes>
-      <Route path="/" element={<ListingsWorkspace />} />
-      <Route path="/overview" element={<ListingOverview />} />
-      <Route path="/new" element={<ListingForm />} />
-      <Route path="/edit/:id" element={<ListingForm />} />
-    </Routes>
-  )
-}
-}
-  )
-}
+    <div className="flex h-full flex-col">
+      <Routes>
+        <Route path="/" element={<ListingsWorkspace />} />
+        <Route path="/overview" element={<ListingOverview />} />
+        <Route path="/new" element={<ListingForm />} />
+        <Route path="/edit/:id" element={<ListingForm />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </div>
   )
 }
