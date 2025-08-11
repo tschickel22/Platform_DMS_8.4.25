b441e9a8e4fab04ffd5851609dee7e060040e244
tschickel22 <109991351+tschickel22@users.noreply.github.com>
2025-08-11 16:07:45 -0600

    Fix export/import mismatch for ListingPreviewsSettings

--- src/modules/company-settings/CompanySettings.tsx
+++ src/modules/company-settings/CompanySettings.tsx
@@ -16,8 +16,8 @@ import { LabelOverrides } from './components/LabelOverrides'
 import { NotificationTemplates } from './components/NotificationTemplates'
 import { IntegrationSettings } from './components/IntegrationSettings'
 import WarrantySettings from './components/WarrantySettings'
-import { mockCompanySettings } from '@/mocks/companySettingsMock'
-import { ListingPreviewsSettings } from './components/ListingPreviewsSettings'
+import ListingPartnersSettings from './components/ListingPartnersSettings'
+import ListingPreviewsSettings from './components/ListingPreviewsSettings'
 
 function CompanySettingsPage() {
   const { tenant, getCustomFields, updateTenant, addCustomField, updateCustomField, deleteCustomField } = useTenant()
