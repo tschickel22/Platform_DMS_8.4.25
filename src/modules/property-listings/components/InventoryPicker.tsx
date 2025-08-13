c918e14ed9e00591719d2933b20cb889d665c375
tschickel22 <109991351+tschickel22@users.noreply.github.com>
2025-08-13 11:06:21 -0600

    Scan Property Listings Wiring

--- src/modules/property-listings/components/InventoryPicker.tsx
+++ src/modules/property-listings/components/InventoryPicker.tsx
@@ -3,7 +3,6 @@ import { useNavigate } from 'react-router-dom';
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
-import { Badge } from '@/components/ui/badge'
 import { Card, CardContent } from '@/components/ui/card'
 import { useCatalog } from '@/data/catalog'
 
