1641097b5b8cf876c4d6d405271da6a0fec3c879
tschickel22 <109991351+tschickel22@users.noreply.github.com>
2025-08-14 08:38:37 -0600

    Fix Vite Configuration White Screen

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
@@ -26,10 +25,5 @@ export default defineConfig({
         }
       }
     }
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
   }
 })
