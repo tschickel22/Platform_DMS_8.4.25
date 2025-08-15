c128eaffaa6b1a34dcd2f195b2ffff5f1083ae28
tschickel22 <109991351+tschickel22@users.noreply.github.com>
2025-08-15 10:51:56 -0600

    Updated QuoteBuilder.tsx

--- src/modules/crm-prospecting/components/QuoteBuilder.tsx
+++ src/modules/crm-prospecting/components/QuoteBuilder.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react'
-import { mockInventory } from '@/mocks/inventoryMock'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
