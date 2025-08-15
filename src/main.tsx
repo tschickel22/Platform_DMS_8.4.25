7a92870d005bfd074d4f0b8dd10fc7f76bb855eb
tschickel22 <109991351+tschickel22@users.noreply.github.com>
2025-08-14 22:28:55 -0600

    Updated package-lock.json

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { HelmetProvider } from 'react-helmet-async'
 import * as pdfjs from 'pdfjs-dist'
 import PdfWorker from 'pdfjs-dist/build/pdf.worker.min.mjs?url'
 import App from './App.tsx'
@@ -9,5 +10,7 @@ import './index.css'
 pdfjs.GlobalWorkerOptions.workerSrc = PdfWorker
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <App />
+  <HelmetProvider>
+    <App />
+  </HelmetProvider>
 )
