1ef28225bed1e36ff512d9f220cb99ba7720ac8a
tschickel22 <109991351+tschickel22@users.noreply.github.com>
2025-08-18 14:30:09 -0600

    Fix Website Builder Index Exports

--- src/modules/website-builder/index.ts
+++ src/modules/website-builder/index.ts
@@ -1,4 +1,3 @@
 export { WebsiteBuilderRoutes, CompanyWebsiteRoutes } from './routes'
-export { WebsiteBuilderShell } from './WebsiteBuilderShell'
 export * from './types'
 export * from './hooks/useSite'
