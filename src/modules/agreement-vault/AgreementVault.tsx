935c97fb8e15bffaca715cafb216763c9a56ef55
tschickel22 <109991351+tschickel22@users.noreply.github.com>
2025-08-30 07:57:01 -0600

    Updated ignore

--- src/modules/agreement-vault/AgreementVault.tsx
+++ src/modules/agreement-vault/AgreementVault.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import { Routes, Route, useNavigate } from 'react-router-dom'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
